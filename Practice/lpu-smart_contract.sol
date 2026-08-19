bf1eafdc5a437dee448725085f8ec6aa727b45bc
premshivaaa <chanumuripremshiva@gmail.com>
2026-08-19 12:39:01 +0530

    Initial Commit

---
+++ Practice/lpu-smart_contract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract lpu_smart_contract{
+    string name = "Hacker";
+    int age = 20;
+    string stream = "CSE";
+    int fees = 86400;
+}
